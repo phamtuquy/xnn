0ad42a8084e91587dcb0de8af552f2b36edc3a45
Quy Pham <tuquy12a3@gmail.com>
2015-04-26 23:15:26 -0400

    embeded code for facebook

--- application/views/layouts/master_page.php
+++ application/views/layouts/master_page.php
@@ -17,6 +17,25 @@
 		<script type="text/javascript" src="<?php echo base_url()?>public/scripts/jquery-2.1.3.min.js"></script>
 		<!-- script src="http://code.jquery.com/jquery-1.11.2.min.js"></script -->
 		<link rel="stylesheet" href="<?php echo base_url()?>public/css/style.css">
+<!--
+<!-- Facebook Conversion Code for Xinh nh? nhàng -->
+<script>(function() {
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+  }
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', '6021945295124', {'value':'1.00','currency':'VND'}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6021945295124&amp;cd[value]=1.00&amp;cd[currency]=VND&amp;noscript=1" /></noscript>
+-->
+
 	</head>
 
 	<body>
